dfcfa5f38a7b75db9516536ace9a4630e5ae94f8
DerKommentator <VWME06U@vw.vwg>
2019-10-27 21:07:55 +0100

    Update funktion_ziffer.php

--- PHP/funktion_ziffer.php
+++ PHP/funktion_ziffer.php
@@ -47,15 +47,23 @@
 
 function number_2_numeral($number)
 {
-    $numerals = ['Null', 'Eins', 'Zwei', 'Drei', 'Vier', 'Fünf', 'Sechs',
-                    'Sieben', 'Acht', 'Neun', 'Zehn'];
-    if($number<10)
+    $pos_numerals = ['Eins', 'Zwei', 'Drei', 'Vier',
+                     'Fünf', 'Sechs', 'Sieben', 'Acht', 'Neun'];
+    $neg_numerals = ['Null', 'minus Eins', 'minus Zwei', 'minus Drei',
+                     'minus Vier', 'minus Fünf', 'minus Sechs',
+                     'minus Sieben', 'minus Acht', 'minus Neun'];
+    if($number > 0)
     {
-        return $numerals[$number];
+        return $pos_numerals[$number-1]; # count -1 because pos_numerals doesn't contains 'Null'
+    }
+    elseif($number < 0)
+    {
+        $number = $number * -1;
+        return $neg_numerals[$number];
     }
     else
     {
-        return 'Dies ist keine Ziffer';
+        return 'Not a number';
     }
 }
 
@@ -63,17 +71,25 @@ function number_2_numeral($number)
 function number_2_digits($number)
 {
 	$output_array = [];
-	while($number >= 1)
-	{
-		array_push($output_array, $number % 10);
-		$number = $number / 10;
-	}
-	$output_array = array_reverse($output_array);
+    if($number != 0)
+    {
+        while($number <= -1 || $number >= 1)
+    	{
+    		array_push($output_array, $number % 10);
+    		$number = $number / 10;
+    	}
+        $output_array = array_reverse($output_array);
+    	return $output_array;
+    }
+    else
+    {
+        return [$number];
+    }
+    #print_r($output_array);
 
-	return $output_array;
 }
 
-$number = 425;
+$number = -545;
 
 echo $number.' als Wort: ';
 foreach(number_2_digits($number) as $digit)
