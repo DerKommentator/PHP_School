39b2c55707ce030704aaf64b3c7d27b1c9e55633
DerKommentator <VWME06U@vw.vwg>
2019-10-28 10:50:37 +0100

    Update array.php

--- PHP/array.php
+++ PHP/array.php
@@ -1,26 +1,91 @@
 <?php
 
-$werte = array(5,60,0,50);
+/*
 $summe = 0;
 
-$werte[] = 7;
-
-$werte[10] = 8;
-
-/*for($i=0; $i<count($werte); $i+=1)
+for($i=0; $i<count($werte); $i+=1)
 {
     $summe = $summe + $werte[$i];
     echo 'Werte['.$i.']='.$werte[$i].'<br>';
 }
 echo 'Die Summe der Werte ist '.$summe.'<br>';
 
-echo '<br><br>';*/
+echo '<br><br>';
+
 
 foreach($werte as $index => $wert)
 {
     echo 'Werte['.$index.']='.$wert.'<br>';
 }
+echo '<hr>';
+*/
 
 
-print_r($werte);
+function ausgabe($array)
+{
+    foreach($array as $index => $element)
+    {
+        echo 'Array['.$index.']='.$element.'<br>';
+    }
+}
+
+function summe($array)
+{
+    $summe = 0;
+    foreach($array as $element)
+    {
+        $summe += $element;
+    }
+    return $summe;
+}
+
+function mittelwert($array)
+{
+    $mittelwert = 0;
+    foreach($array as $element)
+    {
+        $mittelwert = summe($array) / count($array);
+    }
+    return $mittelwert;
+}
+
+function maximum($array)
+{
+    $max_element = 0;
+    foreach($array as $element)
+    {
+        if($element > $max_element)
+        {
+            $max_element = $element;
+        }
+    }
+    return $max_element;
+}
+
+function minimum($array)
+{
+    $min_element = 0;
+    foreach($array as $element)
+    {
+        if($element < $min_element)
+        {
+            $min_element = $element;
+        }
+    }
+    return $min_element;
+}
+
+$werte = array(5,60,1,50,7,8);
+
+
+ausgabe($werte);
+
+echo 'Summe des Arrays ist '.summe($werte).'<br>';
+
+echo 'Mittelwert des Arrays ist '.mittelwert($werte).'<br>';
+
+echo 'Das größte Element ist '.maximum($werte).'<br>';
+
+echo 'Das kleinste Element ist '.minimum($werte).'<br>';
+
 ?>
