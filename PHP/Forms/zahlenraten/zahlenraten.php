87b110e7ec26f7890827a3d26ee35acad0eb6762
DerKommentator <22379432+DerKommentator@users.noreply.github.com>
2019-12-02 08:11:53 +0100

    Update zahlenraten.php

--- PHP/Forms/zahlenraten/zahlenraten.php
+++ PHP/Forms/zahlenraten/zahlenraten.php
@@ -1,20 +1,21 @@
 <!DOYTYPE html>
 <html>
 	<head>
-		<!--<link rel="stylesheet" type="text/css" href="zahlenratenSpiel.css" />-->
+		<link rel="stylesheet" type="text/css" href="zahlenratenSpiel.css" />
 	</head>
-	
+
 	<body>
-		<!--<div id="bgvideo" style="width: 1920px; height: 1080px;">
+		<div id="bgvideo" style="width: 1920px; height: 1080px;">
 			<video id="bgvideo_player" style="width:100%; height:auto;" preload="auto" autoplay="true" loop autobuffer muted="muted" poster="https://ubistatic19-a.akamaihd.net/resource/en-us/game/rainbow6/siege/r6-siege-bg.jpg">
 				<source src="https://ubistatic2-a.akamaihd.net/gamesites/rainbow6/r6-siege-bg_1280.mp4" type="video/mp4">
 				<source src="https://ubistatic2-a.akamaihd.net/gamesites/rainbow6/r6-siege-bg_1280.webm" type="video/webm">
 			</video>
-		</div>-->
-		
+		</div>
+
 		<div id="main">
 				<div class="php">
 					<?php
+
 
 						echo '<h1>Zahlenraten</h1><br>';
 						echo 'Geben Sie eine Zahl zwischen 1 und 100 an:';
@@ -42,12 +43,12 @@
 
 						echo '<form method="post">';
 							echo '<input type="hidden" name="Geheim" value="'.$_POST['Geheim'].'">';
-							
+
 
 							echo '<input type="text" name="eWert" value="'.$_POST['eWert'].'">';
 							echo ' ';
 							echo '<input type="submit" name="tippen" value="Tippen"><br>';
-							
+
 							echo '<input type="hidden" name="versuche" value="'.$_POST['versuche'].'">';
 						echo '</form>';
 
@@ -65,7 +66,7 @@
 							{
 								echo $_POST['eWert'].' ist zu niedrig'.'<br>';
 							}
-							
+
 							elseif($_POST['Geheim'] == $_POST['eWert'])
 							{
 								echo $_POST['eWert'].' ist der richtige Wert'.'<br>';
